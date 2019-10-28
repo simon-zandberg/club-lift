572fc13d27da4e91516f114f9c0ae02e6b8ed13d
Simon Zandberg <simon.zandberg@gmail.com>
2019-10-28 20:06:08 +1100

    App name change

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,7 +5,7 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
     <div className="box-layout">
         <div className="box-layout__box">
-            <h1 className="box-layout__title">Boilerplate</h1>
+            <h1 className="box-layout__title">Club Lift</h1>
             <p>Tag line for app</p>
             <button className="button" onClick={startLogin}>Login with Google</button>
         </div>
